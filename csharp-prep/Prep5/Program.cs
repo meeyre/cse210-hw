a969eab157cfa4e0e64564c4c5a425e50359d0b3
meeyre <eyr21002@byui.edu>
2025-01-16 17:26:50 -0700

    Completed Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,35 @@ using System;
 
 class Program
 {
+
+    static void print_wel()
+    {
+        Console.WriteLine("WELCOME TO THE PROGRAM!");
+    }
+
+    static string get_name()
+    {
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int get_fav_num()
+    {
+        Console.WriteLine("What is your favorite number??");
+        int num = int.Parse(Console.ReadLine());
+        return num;
+    }
+
+    static void print_name_y_sqnum(string name, int number)
+    {
+        Console.WriteLine($"Hello {name}, your squared number is {number*number}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        print_wel();
+        print_name_y_sqnum(get_name(), get_fav_num());
     }
+
 }
